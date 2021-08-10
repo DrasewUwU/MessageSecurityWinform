b0aa6e8a4fb3f0a08620651a9b0c51f1e2073c05
Trần Ngọc Tiến <ngoctienTNT>
2021-08-10 18:55:26 +0700

    first commit

---
+++ Message Security/Message Security/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace Message_Security
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Option = new System.Windows.Forms.ToolStripMenuItem();
+            this.backGroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textcolorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(368, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mã Hóa";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(368, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(368, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(368, 205);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 27);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.label1.Location = new System.Drawing.Point(300, 102);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ":Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.GhostWhite;
+            this.label2.Location = new System.Drawing.Point(162, 152);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.MinimumSize = new System.Drawing.Size(180, 20);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(200, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ":Tin Nhắn Cần Mã Hóa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.GhostWhite;
+            this.label3.Location = new System.Drawing.Point(171, 205);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.MinimumSize = new System.Drawing.Size(171, 20);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(190, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = ":Tin Nhắn Đã Mã Hóa";
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(498, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 30);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Cài Lại";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(389, 295);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Chuyển Sang Dịch";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Option});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 31);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Option
+            // 
+            this.Option.BackColor = System.Drawing.Color.Transparent;
+            this.Option.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backGroundToolStripMenuItem,
+            this.colorTextToolStripMenuItem,
+            this.textcolorToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.Option.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Option.ForeColor = System.Drawing.Color.White;
+            this.Option.Name = "Option";
+            this.Option.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.Option.Size = new System.Drawing.Size(80, 27);
+            this.Option.Text = "Option";
+            this.Option.Click += new System.EventHandler(this.settingToolStripMenuItem_Click);
+            // 
+            // backGroundToolStripMenuItem
+            // 
+            this.backGroundToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.backGroundToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backGroundToolStripMenuItem.Image = global::Message_Security.Properties.Resources.icons8_opacity_80;
+            this.backGroundToolStripMenuItem.Name = "backGroundToolStripMenuItem";
+            this.backGroundToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.backGroundToolStripMenuItem.Size = new System.Drawing.Size(245, 28);
+            this.backGroundToolStripMenuItem.Text = "Background";
+            this.backGroundToolStripMenuItem.Click += new System.EventHandler(this.backGroundToolStripMenuItem_Click);
+            // 
+            // colorTextToolStripMenuItem
+            // 
+            this.colorTextToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.colorTextToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorTextToolStripMenuItem.Image = global::Message_Security.Properties.Resources.font;
+            this.colorTextToolStripMenuItem.Name = "colorTextToolStripMenuItem";
+            this.colorTextToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.colorTextToolStripMenuItem.Size = new System.Drawing.Size(245, 28);
+            this.colorTextToolStripMenuItem.Text = "LabelColor";
+            this.colorTextToolStripMenuItem.Click += new System.EventHandler(this.colorTextToolStripMenuItem_Click);
+            // 
+            // textcolorToolStripMenuItem
+            // 
+            this.textcolorToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textcolorToolStripMenuItem.Image = global::Message_Security.Properties.Resources.text__2_;
+            this.textcolorToolStripMenuItem.Name = "textcolorToolStripMenuItem";
+            this.textcolorToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this.textcolorToolStripMenuItem.Size = new System.Drawing.Size(245, 28);
+            this.textcolorToolStripMenuItem.Text = "TextColor";
+            this.textcolorToolStripMenuItem.Click += new System.EventHandler(this.textcolorToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutToolStripMenuItem.Image = global::Message_Security.Properties.Resources.icons8_info_1000;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.G)));
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(245, 28);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitToolStripMenuItem.Image = global::Message_Security.Properties.Resources.tải_xuống__1_;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(245, 28);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Message_Security.Properties.Resources._764967;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(818, 497);
+            this.MinimumSize = new System.Drawing.Size(818, 497);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Message Security";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Option;
+        private System.Windows.Forms.ToolStripMenuItem backGroundToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textcolorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Message Security/Message Security/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Message_Security
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.BackColor = System.Drawing.Color.Transparent;
+            label2.BackColor = System.Drawing.Color.Transparent;
+            label3.BackColor = System.Drawing.Color.Transparent;
+            if (Option.ForeColor == Color.Black) Option.ForeColor = Color.White;
+        }
+
+        bool kt = true;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string key = textBox1.Text;
+            string message = textBox2.Text;
+            if (message == "") MessageBox.Show("Vui lòng nhập văn bản vào!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            int s = 0; 
+            for (int i=0;i<key.Length;i++) s += (int)key[i];
+            s %= 94 - 46;
+            string messageSecurity=""; 
+            if (kt)
+            {
+                for (int i = 0; i < message.Length; i++)
+                {
+                    int k = (int)message[i];
+                    k += s;
+                    if (k < 32)
+                    {
+                        int t = 32 - k;
+                        k = 127 - t;
+                    }
+                    else if (k > 126)
+                    {
+                        int t = k - 126;
+                        k = 31 + t;
+                    }
+                    char ch = (char)(k);
+                    messageSecurity += ch;
+                }
+                textBox3.Text = messageSecurity;
+            }
+            else
+            {
+                for (int i = 0; i < message.Length; i++)
+                {
+                    int k = (int)message[i];
+                    k -= s;
+                    if (k < 32)
+                    {
+                        int t = 32 - k;
+                        k = 127 - t;
+                    }
+                    else if (k > 126)
+                    {
+                        int t = k - 126;
+                        k = 31 + t;
+                    }
+                    char ch = (char)(k);
+                    messageSecurity += ch;
+                }
+                textBox3.Text = messageSecurity;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            if (kt)
+            {
+                button1.Text = "Giải Hóa";
+                button3.Text = "Chuyển Sang Mã Hóa";
+                label2.Text = ":Tin Nhắn Đã Mã Hóa";
+                label3.Text = ":Tin Nhắn Đã Giải Hóa";
+            }
+            else
+            {
+                button1.Text = "Mã Hóa";
+                button3.Text = "Chuyển Sang Dịch";
+                label2.Text = ":Tin Nhắn Cần Mã Hóa";
+                label3.Text = ":Tin Nhắn Đã Mã Hóa";
+            }    
+            kt = !kt;
+        }
+
+        private void backGroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog()==DialogResult.OK)
+            {
+                string fileName = dlg.FileName;
+                Image myImage = new Bitmap(fileName);
+                this.BackgroundImage = myImage;
+            }    
+        }
+
+        private void colorTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog()==DialogResult.OK)
+            {
+                string str = dlg.Color.Name;
+                label1.ForeColor = dlg.Color;
+                label2.ForeColor = dlg.Color;
+                label3.ForeColor = dlg.Color;
+            }    
+        }
+
+        private void textcolorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string str = dlg.Color.Name;
+                textBox1.ForeColor = dlg.Color;
+                textBox2.ForeColor = dlg.Color;
+                textBox3.ForeColor = dlg.Color;
+            }
+        }
+
+        bool optionColor = true;
+        private void settingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (optionColor) Option.ForeColor = Color.Black;
+            else Option.ForeColor=Color.White;
+            optionColor = !optionColor;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Option.ForeColor = Color.White;
+            optionColor = true;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Phiên bản: 1.0\nĐây là ứng dụng dùng để mã hóa tin nhắn\nĐược thiết kế bởi Trần Ngọc Tiến", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void VietSub(ref string str)
+        {
+            if (str == "â") str = "aa";
+            else if (str == "Â") str = "AA";
+            else if (str == "ă") str = "aw";
+            else if (str == "Ă") str = "AW";
+        }    
+
+        private void ChangeText(ref string str)
+        {
+            string text = "";
+            for (int i = 0; i < str.Length; i++)
+                text += str[i];
+            str = text;
+        }
+    }
+}
